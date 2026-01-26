1bbacb35fa1ba8842715d249466e860b38cada51
jacob <jacobbaumei@gmail.com>
2026-01-25 19:40:46 -0500

    inbocie vui

--- src/pages/EmailListPage.tsx
+++ src/pages/EmailListPage.tsx
@@ -2,8 +2,8 @@ import { EmailSignupBand } from '../components/EmailSignupBand';
 
 export function EmailListPage() {
   return (
-    <div className="min-h-screen bg-[#F8F5F0]">
-      <EmailSignupBand withBackground={false} />
+    <div className="h-full bg-[#F8F5F0]">
+      <EmailSignupBand withBackground={false} sectionClassName="py-10 md:py-16" />
     </div>
   );
 }
